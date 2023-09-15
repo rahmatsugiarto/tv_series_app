9237541653803d23105d93789a74d5bc5b12550c
rahmatsugiarto <sugiartorahmat187@gmail.com>
2023-09-15 20:33:31 +0700

    update flutter and fix stateless widget

--- lib/presentation/screen/detail/detail_screen.dart
+++ lib/presentation/screen/detail/detail_screen.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
-import '../../../models/tv_series.dart';
-import 'mobile_detail_view.dart';
-import '../../widgets/responsive.dart';
 
+import '../../../models/tv_series.dart';
+import '../../widgets/responsive.dart';
+import 'mobile_detail_view.dart';
 import 'tablet_desktop_detail_view.dart';
 
-class DetailScreen extends StatefulWidget {
+class DetailScreen extends StatelessWidget {
   final TvSeries tvSeries;
   const DetailScreen({super.key, required this.tvSeries});
 
   @override
-  State<DetailScreen> createState() => _DetailScreenState();
-}
-
-class _DetailScreenState extends State<DetailScreen> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Responsive(
-        mobile: MobileDetailView(tvSeries: widget.tvSeries),
-        tablet: TabletDesktopDetailView(tvSeries: widget.tvSeries),
-        desktop: TabletDesktopDetailView(tvSeries: widget.tvSeries),
+        mobile: MobileDetailView(tvSeries: tvSeries),
+        tablet: TabletDesktopDetailView(tvSeries: tvSeries),
+        desktop: TabletDesktopDetailView(tvSeries: tvSeries),
       ),
     );
   }
